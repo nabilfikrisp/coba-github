d4eaee0770b835da1b7da710ff7befb836d6fc3e
nabilfikrisp <79518623+nabilfikrisp@users.noreply.github.com>
2024-05-22 19:20:43 +0700

    feat: perkalian (#3)

--- main.js
+++ main.js
@@ -7,6 +7,10 @@ function pengurangan(num1, num2) {
   return num1 - num2;
 }
 
+function perkalian(num1, num2) {
+  return num1 * num2;
+}
+
 function pembagian(num1, num2) {
   return num1 / num2;
 }
